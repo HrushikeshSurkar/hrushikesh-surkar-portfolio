05266f727eedb71230cef56f0c119bec3e56cd7c
Hrushikesh <hrushikesh.surkar@greatplaceitservices.com>
2024-02-10 20:02:55 +0530

    Hero section added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,18 @@
 import React from "react";
-import About from "./About";
 import "../styles/pages/Home.css";
+
 const HeroSection = () => {
   return (
     <section className="hero">
       <div className="hero-content">
-        <h1>Hi, I'm Hrushikesh Surkar</h1>
-        <p>Let's Code Together ..!</p>
-        <button>View Projects</button>
+        <h1 className="hero-title">
+          <span>Welcome</span> to My Portfolio
+        </h1>
+        <p className="hero-subtitle">Explore my work and projects</p>
+        <button className="hero-button">View Projects</button>
       </div>
     </section>
   );
 };
 
-const Home = () => {
-  return (
-    <div className="home">
-      {/* Hero Section */}
-      <HeroSection />
-      {/* About Section */}
-      {/* <About /> */}
-      {/* ... other sections (Skills, Projects, Contact) */}
-    </div>
-  );
-};
-
-export default Home;
+export default HeroSection;
